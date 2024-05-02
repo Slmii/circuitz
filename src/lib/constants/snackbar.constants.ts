f89352eaa46621dfabc8a993d8638653d3e06f8b
Slmii <selami.c@sbytes-it.com>
2024-05-03 01:48:44 +0200

    remove unused code

--- src/lib/constants/snackbar.constants.ts
+++ src/lib/constants/snackbar.constants.ts
@@ -1,4 +1,2 @@
 export const AUTO_HIDE_DURATION = 5000;
 export const MUTATE_ERROR = 'Looks like something went wrong!';
-export const PLUG_WALLET_INSTALL = 'Please make sure you have PLUG wallet installed';
-export const BITFINITY_WALLET_INSTALL = 'Please make sure you have PLUG wallet installed';
